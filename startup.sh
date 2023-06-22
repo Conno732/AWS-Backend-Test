6b573b296652c2564585997dec0d0552805b4b3d
Connor W <connorscottwalters@gmail.com>
2023-06-22 14:48:10 -0400

    Modify sh file

--- startup.sh
+++ startup.sh
@@ -12,6 +12,11 @@ sudo git clone https://github.com/Conno732/AWS-Backend-Test.git
 cd AWS-Backend-Test/
 sudo python3 manage.py makemigrations
 sudo python3 manage.py migrate
+sudo usermod -a -G www-data ubuntu
+sudo chown -R ubuntu:www-data /var/www
+sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
+find /var/www -type f -exec sudo chmod 0664 {} \;
+
 
 ###
 sudo bash -c "echo '  
